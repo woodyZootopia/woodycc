9bb0678b215e66e6a46012aea8ac934e5ad47225
woodyZootopia <piequalsabout314159@gmail.com>
2020-03-02 22:40:08 +0900

    fix typo.

--- test.sh
+++ test.sh
@@ -70,8 +70,8 @@ try "int foobar(int x,int y){return x+y;} int main(){return foobar(1,2);}" 3 "fo
 try "int foobar(int x,int y){return x+y;} int main(){int a; a = 2;return foobar(1,a);}" 3 "foo.o"
 try 'int foobar(int x){if(x!=1) return x; return 0;} int main(){return foobar(3);}' 3
 try "int fib(int x){if(x==0)return 1;if(x==1)return 1; return fib(x-1)+fib(x-2);} int main(){return fib(10);}" 89
-try "int powerroftwo(int x){if(x==0)return 1; return 2*powerroftwo(x-1);} int main(){return powerroftwo(5);}" 32
-try "int powerroftwo(int x,int y){while(x!=0){y=y*2;x=x-1;} return y;} int main(){return powerroftwo(5,1);}" 32
+try "int poweroftwo(int x){if(x==0)return 1; return 2*poweroftwo(x-1);} int main(){return poweroftwo(5);}" 32
+try "int poweroftwo(int x,int y){while(x!=0){y=y*2;x=x-1;} return y;} int main(){return poweroftwo(5,1);}" 32
 
 # test multiple character local variable
 echo "LINENO:$LINENO"
